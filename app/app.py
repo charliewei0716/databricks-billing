2b52d8a48beb696569b35c84a3b7554bf1d69ccb
Charlie Wei <91643249+charliewei0716@users.noreply.github.com>
2025-04-27 04:26:13 +0000

    Update

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+st.set_page_config(layout="wide")
+
+st.header("Hello world!!!")
+
+st.header("")
+
